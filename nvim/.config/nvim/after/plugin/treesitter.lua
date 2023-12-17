a4edec0bdcf05bc7b39a22e43862573994085e3d
Yanis Fourel <yanis.fourel@ledr.io>
2023-12-17 09:29:01 +0800

    nvim: add hypr treesitter grammar

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -17,3 +17,13 @@ require'nvim-treesitter.configs'.setup {
     "yaml",
   },
 }
+
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+parser_config.hypr = {
+  install_info = {
+    url = 'https://github.com/luckasRanarison/tree-sitter-hypr',
+    files = { 'src/parser.c' },
+    branch = 'master',
+  },
+  filetype = 'hypr',
+}
