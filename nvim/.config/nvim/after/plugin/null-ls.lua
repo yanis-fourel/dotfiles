71723248645fe5303126aaf35cea6a3454e81475
Yanis Fourel <yanis.fourel@ledr.io>
2024-02-06 13:57:15 +0800

    nvim: add black autoformat + pyright to ignore some problematic type checks

--- nvim/.config/nvim/after/plugin/null-ls.lua
+++ nvim/.config/nvim/after/plugin/null-ls.lua
@@ -7,11 +7,14 @@ null_ls.setup({
 		-- TODO: check that: null_ls.builtins.formatting.autopep8
 		-- TODO:    or that: null_ls.builtins.formatting.black
 
-		-- GO
+		-- Go
         null_ls.builtins.formatting.gofmt,
         -- null_ls.builtins.formatting.goimports_reviser
 
-		-- RUST
+		-- Python
+		null_ls.builtins.formatting.black,
+
+		-- Rust
 		null_ls.builtins.formatting.rustfmt,
     },
 })
--- nvim/.config/nvim/lua/dz/lsp/python.lua
+++ nvim/.config/nvim/lua/dz/lsp/python.lua
@@ -2,14 +2,29 @@ local lspconfig = require('lspconfig')
 
 local M = {}
 
+local augroup = vim.api.nvim_create_augroup("BlackAutoFmt", {})
+
 M.setup = function(capabilities)
 	lspconfig.pyright.setup({
 		capabilities = capabilities,
 		settings = {
 			python = {
-
+				analysis = {
+					diagnosticSeverityOverrides = {
+						reportInvalidTypeForm = 'none', -- causes problem with avial types
+					}
+				},
 			}
-		}
+		},
+		on_attach = function (_, bufnr)
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end
+			})
+		end
 	})
 end
 
