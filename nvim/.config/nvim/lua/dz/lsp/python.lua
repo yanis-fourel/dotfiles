c29712aee8ebe316131c1d2a000fe82bec23612d
Yanis Fourel <yanis.fourel@ledr.io>
2024-02-09 16:21:10 +0800

    nvim: python: setup pylint

--- nvim/.config/nvim/lua/dz/lsp/python.lua
+++ nvim/.config/nvim/lua/dz/lsp/python.lua
@@ -5,17 +5,55 @@ local M = {}
 local augroup = vim.api.nvim_create_augroup("BlackAutoFmt", {})
 
 M.setup = function(capabilities)
-	lspconfig.pyright.setup({
+
+	local venv = os.getenv("VIRTUAL_ENV")
+	local pylint_executable = "pylint"
+	if venv ~= nil then
+		if vim.fn.executable(venv .. "/bin/pylint") == 1 then
+			pylint_executable = venv .. "/bin/pylint"
+		else
+			vim.notify("pylint not found in virtual environment: " .. venv .. ". It may not resolve depencies correctly. `pip install pylint` in your venv", vim.log.levels.WARN)
+		end
+	end
+
+	vim.notify("Using pylint executable: " .. pylint_executable, vim.log.levels.INFO)
+
+	lspconfig.pylsp.setup({
 		capabilities = capabilities,
-		settings = {
-			python = {
-				analysis = {
-					diagnosticSeverityOverrides = {
-						reportInvalidTypeForm = 'none', -- causes problem with avial types
+        settings = {
+			pylsp = {
+				plugins = {
+					autopep8 = {
+						enabled = false,
+					},
+					flake8 = {
+						enabled = false,
+					},
+					mccabe = { -- automatically detects over-complex code basing on cyclomatic complexity
+						enabled = true,
+					},
+					preload = { -- give feedback while importing heavy modules
+						enabled = false,
+					},
+					pycodestyle = {
+						enabled = false, -- we use black
+					},
+					pydocstyle = {
+						enabled = false,
+					},
+					pyflakes = {
+						enabled = false,
+					},
+					pylint = {
+						enabled = true,
+						executable = pylint_executable,
+					},
+					yapf = {
+						enabled = false,
 					}
-				},
+				}
 			}
-		},
+        },
 		on_attach = function (_, bufnr)
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				group = augroup,
