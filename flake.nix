43bb61d9085266888bb1dc18d69c38f24c9ad146
Yanis Fourel <yanis.fourel@gmail.com>
2025-01-01 19:46:51 +0100

    203 current  2024-12-30 08:21:38  25.05.20241219.d70bd19   6.6.66                          *

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Yanis system";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    mynvim.url = "path:./nvim/";
+
+    # rust toolchain
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ghostty = {
+      url = "github:ghostty-org/ghostty";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      nixpkgs-unstable,
+      fenix,
+      ghostty,
+      ...
+    }@inputs:
+    {
+      packages.x86_64-linux.default = fenix.packages.x86_64-linux.minimal.toolchain;
+
+      nixosConfigurations.yanix = nixpkgs.lib.nixosSystem {
+        specialArgs = rec {
+          system = "x86_64-linux";
+          inherit inputs;
+          pkg_ghostty = ghostty.packages.${system}.default;
+        };
+        modules = [ ./nix/hosts/yanix/config.nix ];
+      };
+
+      nixosConfigurations.ledr-yanix = nixpkgs.lib.nixosSystem {
+        specialArgs = rec {
+          system = "x86_64-linux";
+          inherit inputs;
+          pkg_ghostty = ghostty.packages.${system}.default;
+        };
+        modules = [ ./nix/hosts/ledr-yanix/config.nix ];
+      };
+    };
+}
