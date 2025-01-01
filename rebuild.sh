732b4e4952dfa1e815a21fc4349337f10f11b5df
Yanis Fourel <yanis.fourel@gmail.com>
2025-01-01 19:49:39 +0100

    add nixhost concept

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,10 @@
 # Based on NoBoilerplates' dotfiles. I love his youtube channel
-
 set -e
+
+nixhost=$(cat nixhost)
 git add nix flake.nix flake.lock
 sudo nix flake lock
-echo "NixOS Rebuilding..."
-sudo nixos-rebuild --flake .#yanix switch # || (cat nixos-switch.log | grep --color error && false)
+echo "NixOS Rebuilding and switching for host $nixhost"
+sudo nixos-rebuild --flake ".#$nixhost" switch # || (cat nixos-switch.log | grep --color error && false)
 gen=$(nixos-rebuild list-generations | grep current)
 git commit -m "$gen"
