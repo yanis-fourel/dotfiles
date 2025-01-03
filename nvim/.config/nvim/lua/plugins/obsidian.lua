427c78e3cea0b8376cac0d34fe497d20725c1084
Yanis Fourel <yanis.fourel42@proton.me>
2025-01-03 10:08:42 +0100

    nvim: add obsidian.lua

---
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,69 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	ft = "markdown",
+	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	-- event = {
+	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+	--   -- refer to `:h file-pattern` for more examples
+	--   "BufReadPre path/to/my-vault/*.md",
+	--   "BufNewFile path/to/my-vault/*.md",
+	-- },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		require("obsidian").setup({
+			workspaces = {
+				{
+					name = "second brain",
+					path = "~/Sync/Obsidian/Second brain/",
+				},
+			},
+			completion = {
+				-- Set to false to disable completion.
+				nvim_cmp = true,
+				-- Trigger completion at 2 chars.
+				min_chars = 2,
+			},
+			mappings = {
+				-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+				["gf"] = {
+					action = function()
+						return require("obsidian").util.gf_passthrough()
+					end,
+					opts = { noremap = false, expr = true, buffer = true },
+				},
+				-- Toggle check-boxes.
+				["<leader>ch"] = {
+					action = function()
+						return require("obsidian").util.toggle_checkbox()
+					end,
+					opts = { buffer = true },
+				},
+				-- Smart action depending on context, either follow link or toggle checkbox.
+				["<cr>"] = {
+					action = function()
+						return require("obsidian").util.smart_action()
+					end,
+					opts = { buffer = true, expr = true },
+				},
+			},
+			disable_frontmatter = true,
+		})
+
+		local vault_location = "/home/yanis/Sync/Obsidian/Second brain/**.md"
+		local group = vim.api.nvim_create_augroup("obsidian_cmds", { clear = true })
+		vim.api.nvim_create_autocmd("BufEnter", {
+			command = "ObsidianOpen",
+			pattern = { vault_location },
+			group = group,
+			desc = "Opens the current buffer in Obsidian",
+		})
+
+		vim.notify("ok, setup obsidian command group")
+		vim.o.conceallevel = 2
+	end,
+}
