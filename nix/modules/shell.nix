09e63eb005e534d39290899ba9f8ce481b00c483
Yanis Fourel <yanis.fourel@ledr.io>
2025-01-08 11:29:30 +0100

    ledr-yanix: 25 current  2025-01-08 11:29:28  25.05.20241219.d70bd19   6.6.66                          *

--- nix/modules/shell.nix
+++ nix/modules/shell.nix
@@ -3,19 +3,12 @@
   programs.zsh = {
     enable = true;
     autosuggestions.enable = true;
-    ohMyZsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "rust"
-      ];
-    };
   };
 
   environment.systemPackages = [
     pkgs.nushell
     pkgs.starship
-    pkgs.eza
+    pkgs.carapace
   ];
 
 }
