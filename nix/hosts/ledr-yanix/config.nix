c8f13ad58928365c2ac8083afa9fcdcebac83762
Yanis Fourel <yanis.fourel@ledr.io>
2025-02-17 16:07:35 +0800

    ledr-yanix: 34 current  2025-02-17 16:07:11  25.05.20241219.d70bd19   6.6.66                          *

--- nix/hosts/ledr-yanix/config.nix
+++ nix/hosts/ledr-yanix/config.nix
@@ -32,6 +32,13 @@ pkgs,
     pkgs.clamtk
   ];
 
+  services = {
+    clamav = {
+      daemon. enable = true;
+      updater.enable = true;
+    };
+  };
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
