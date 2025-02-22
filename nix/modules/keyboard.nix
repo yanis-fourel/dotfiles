4ad22763c62c0ea7e3febe3b48bba124a9d58238
Yanis Fourel <yanis.fourel42@proton.me>
2025-02-22 15:30:42 +0800

    yanix: 219 current  2025-02-22 15:30:30  25.05.20241219.d70bd19   6.6.66                          *

--- nix/modules/keyboard.nix
+++ nix/modules/keyboard.nix
@@ -17,11 +17,14 @@ in
   i18n.inputMethod = {
     type = "fcitx5";
     enable = true;
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-      fcitx5-chewing
-      fcitx5-hangul
-    ];
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = [
+        pkgs.fcitx5-mozc
+        pkgs.fcitx5-gtk
+        pkgs.fcitx5-chewing
+        pkgs.fcitx5-hangul
+      ];
+    };
   };
 }
