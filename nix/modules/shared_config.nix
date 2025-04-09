7f2372ec322dee503a3a44a70e1ddd936f9a6688
Yanis Fourel <yanis.fourel@ledr.io>
2025-04-09 15:22:27 +0800

    ledr-yanix: 40 current  2025-04-09 15:22:24  25.05.20241219.d70bd19   6.6.66                          *

--- nix/modules/shared_config.nix
+++ nix/modules/shared_config.nix
@@ -218,7 +218,7 @@
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  time.timeZone = "Asia/Taipei";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
