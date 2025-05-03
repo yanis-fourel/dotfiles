3a27066d6183037cea8aa846acc5cd479a0ef131
Yanis Fourel <yanis.fourel42@proton.me>
2025-05-03 21:56:48 +0800

    yanix: 234 current  2025-05-03 21:56:34  25.05.20250412.2631b0b   6.12.23                          *

--- nix/modules/wireshark.nix
+++ nix/modules/wireshark.nix
@@ -1,10 +1,14 @@
 {pkgs, ...}:
 {
+  programs.wireshark = {
+    enable = true;
+    dumpcap.enable = true; # Allow non-root users to capture packets
+  };
+
   environment.systemPackages = [
     pkgs.wireshark-cli
   ];
 
-  users.groups.wireshark = {};
   users.users.yanis.extraGroups = [ "wireshark" ];
 }
 
