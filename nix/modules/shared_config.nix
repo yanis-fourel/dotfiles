569af938493b72e2900fe6c153ef9e12ca076bd2
Yanis Fourel <yanis.fourel42@proton.me>
2025-07-30 18:59:02 +0900

    yanix: 251 current  2025-07-30 18:58:47  25.11.20250607.3e3afe5   6.12.32                          *

--- nix/modules/shared_config.nix
+++ nix/modules/shared_config.nix
@@ -228,7 +228,7 @@
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Set your time zone.
-  time.timeZone = "Asia/Taipei";
+  time.timeZone = "Asia/Tokyo";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
