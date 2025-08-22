9f54d88e1da4404a0e81f0b81d76e84e08fbd6fe
Yanis Fourel <yanis.fourel42@proton.me>
2025-08-23 07:44:26 +0900

    pkgs unstable -> 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Yanis system";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
     # rust toolchain
