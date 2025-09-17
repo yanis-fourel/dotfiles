1274d08442b5d34020ccde96bfdb4b3652124503
Yanis Fourel <yanis.fourel42@proton.me>
2025-09-17 18:01:58 +0700

    yanix: 276 current  2025-09-17 18:01:42  25.05.20250820.9cb344e   6.12.42                          *

--- nix/modules/shared_config.nix
+++ nix/modules/shared_config.nix
@@ -239,6 +239,8 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  users.extraGroups.networkmanager.members = [ "yanis" ];
+
 
   # Set your time zone.
   time.timeZone = "Asia/Bangkok";
