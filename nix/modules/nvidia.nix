29f4239b7db68e7bf932a054bba56c09851473fb
Yanis Fourel <yanis.fourel42@proton.me>
2025-11-02 19:20:45 +0900

    yanix: 316 current  2025-11-02 19:20:24  25.05.20250820.9cb344e   6.12.42                          *

---
+++ nix/modules/nvidia.nix
@@ -0,0 +1,62 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  hardware.graphics = {
+    enable = true;
+    extraPackages = [
+      pkgs.mesa
+      pkgs.cudaPackages.cudatoolkit  # Installs the CUDA toolkit; use cudaPackages_12.cudatoolkit for a specific version if required.
+    ];
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    # Modesetting is required
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/supend to fail.
+    # Enable this is you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the base essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independant third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommanded setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      # Found using `lshw -c display`
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  # TODO: recompilation takes forever and uses all my ram
+  # nixpkgs.config.cudaSupport = true;  # Enables CUDA globally (may trigger recompilations for some packages).
+
+  
+}
--- nix/modules/shared_config.nix
+++ nix/modules/shared_config.nix
@@ -16,6 +16,7 @@ in
     ./devtools.nix
     ./docker.nix
     ./wireshark.nix
+    ./nvidia.nix
   ];
 
   users.users.yanis = {
@@ -201,58 +202,9 @@ in
   hardware.opentabletdriver.enable = true;
   hardware.opentabletdriver.daemon.enable = true;
 
-  # Needed for NVIDIA, might want to only allow unfree NVIDIA
   nixpkgs.config.allowUnfree = true;
 
-  hardware.graphics = {
-    enable = true;
-    extraPackages = [
-      pkgs.mesa
-    ];
-  };
 
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-    # Modesetting is required
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/supend to fail.
-    # Enable this is you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the base essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independant third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommanded setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-      # Found using `lshw -c display`
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
-    };
-  };
 
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
