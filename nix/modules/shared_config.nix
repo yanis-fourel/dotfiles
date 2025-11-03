98b0111b730405f6492f9207760a76b406d78047
Yanis Fourel <yanis.fourel42@proton.me>
2025-11-03 19:59:01 +0900

    yanix: 321 current  2025-11-03 19:58:43  25.05.20250820.9cb344e   6.12.42                          *

--- nix/modules/shared_config.nix
+++ nix/modules/shared_config.nix
@@ -28,7 +28,7 @@ in
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FShS5r688iDXLoMkBAyZUnDQt4Iv+2qTalRl8="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
