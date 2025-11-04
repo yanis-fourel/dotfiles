8ee362a676b0d1610f51e7e0c64298949d9b59f0
Yanis Fourel <yanis.fourel42@proton.me>
2025-11-04 09:14:41 +0900

    yanix: 322 current  2025-11-04 09:14:21  25.05.20250820.9cb344e   6.12.42                          *

--- nix/hosts/yanix/config.nix
+++ nix/hosts/yanix/config.nix
@@ -33,7 +33,7 @@
     upkgs.anki
     pkgs.osu-lazer-bin
     pkgs.openai-whisper
-    # pkgs.minecraft # currently broken, how to fix?
+    pkgs.hmcl # minecraft launcher
   ];
   # users.groups.plugdev = {};
   users.users.yanis.extraGroups = [ "plugdev" ];
