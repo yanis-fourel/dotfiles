eaa9c4512ea6e4f1dad018c1d768b35bf0d119dc
Yanis Fourel <yanis.fourel42@proton.me>
2025-11-17 12:59:37 +0800

    yanix: 328 current  2025-11-17 12:59:36  25.05.20250820.9cb344e  6.12.42                          *

--- nix/modules/shared_config.nix
+++ nix/modules/shared_config.nix
@@ -291,7 +291,10 @@ in
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts= [ 38777 ]; # temporary minecraft lan
+  };
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
