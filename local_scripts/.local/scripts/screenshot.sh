3d5b178c8757f702ad5447ac2d3144082569267e
Yanis Fourel <yanis.fourel42@proton.me>
2026-03-21 07:49:46 +0900

    local_scripts: add screenshot.sh

---
+++ local_scripts/.local/scripts/screenshot.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Take a screenshot of the whole screen, a specific window, or a user-drawn region.
+# Saves to ~/Pictures
+
+OUTPUT_DIR="$HOME/Pictures"
+
+FILENAME="screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png"
+FILEPATH="$OUTPUT_DIR/$FILENAME"
+
+if [[ ! -d $OUTPUT_DIR ]]; then
+  notify-send "Screenshot directory does not exist: $OUTPUT_DIR" -u critical -t 3000
+  exit 1
+fi
+
+pkill slurp && exit 0
+
+
+hyprpicker -r -z >/dev/null 2>&1 &
+PID=$!
+trap 'kill "$PID" 2>/dev/null || true' EXIT
+sleep .1
+SELECTION=$(slurp 2>/dev/null)
+grim -g "$SELECTION" "$FILEPATH" || exit 1
+[[ -z $SELECTION ]] && exit 0
+kill $PID 2>/dev/null
+trap - EXIT
+
+wl-copy <"$FILEPATH"
+
+satty --filename "$FILEPATH" \
+  --output-filename "$FILEPATH" \
+  --actions-on-enter save-to-clipboard \
+  --save-after-copy \
+  --copy-command 'wl-copy'
