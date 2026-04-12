c4990017ce3fa56b6932b2d6f01ed59f15da1353
Yanis Fourel <yanis.fourel42@proton.me>
2026-04-12 20:41:10 +0900

    nvim: copilot now has proper keybinds

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -3,7 +3,6 @@ return {
 	event = "InsertEnter",
 	version = "1.*",
 	dependencies = {
-		"fang2hou/blink-copilot",
 		-- Snippet Engine & friendly-snippets (exactly like before)
 		{
 			"L3MON4D3/LuaSnip",
@@ -77,18 +76,12 @@ return {
 				"path",
 				"snippets",
 				"buffer",
-				"copilot",
 				"avante_commands",
 				"avante_mentions",
 				"avante_files",
 				"avante_shortcuts",
 			},
 			providers = {
-				copilot = {
-					name = "copilot",
-					module = "blink-copilot",
-					async = true,
-				},
 				avante_commands = {
 					name = "avante_commands",
 					module = "blink.compat.source",
--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -6,36 +6,20 @@ return {
 		server = {
 			settings = {
 				["*"] = {
-					filetypes = {
-						"javascript",
-						"typescript",
-						"python",
-						"lua",
-						"rust",
-						"go",
-						"java",
-						"c",
-						"cpp",
-						"ruby",
-						"php",
-						"swift",
-						"kotlin",
-						"scala",
-						"html",
-						"css",
-						"scss",
-						"json",
-						"yaml",
-						"toml",
-						"sql",
-						"shell",
-						"bash",
-						"zsh",
-						"fish",
-						"dockerfile",
-						"nix",
-					},
+					-- stylua: ignore
+					filetypes = { "javascript", "typescript", "python", "lua", "rust", "go", "java", "c", "cpp", "ruby", "php", "swift", "kotlin", "scala", "html", "css", "scss", "json", "yaml", "toml", "markdown", "sql", "shell", "bash", "zsh", "fish", "dockerfile", "nix" },
 				},
+			},
+		},
+		suggestion = {
+			auto_trigger = true,
+			keymap = {
+				accept = "<Right>",
+			},
+		},
+		panel = {
+			keymap = {
+				accept = "<Right>",
 			},
 		},
 	},
